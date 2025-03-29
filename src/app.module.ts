58d810e4a80855ecd0184174a770565f180dcfdd
Emiliano Portillo <emiliano.portillo1331@gmail.com>
2025-03-28 22:27:06 -0600

    Feat: Aws resource implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { AwsModule } from './aws/aws.module';
 
 
 @Module({
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     MongooseModule.forRoot(process.env.MONGODB),
     AuthModule,
+    AwsModule,
 
   ],
 
